b7e501b5a1fdebc88dcbc2420ca30d33454055d2
Kane Milligan <kane.milligan@glean.co>
2023-04-06 20:47:26 +0100

    set up base project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CalculatorContainer from './components /CalculatorContainer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <CalculatorContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components /CalculatorContainer.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+
+const CalculatorContainer: FunctionComponent = () => {
+    return (
+        <section className="container">
+        </section>
+    )
+}
+
+export default CalculatorContainer
